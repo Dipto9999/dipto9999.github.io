3f5c177223dfcfb4fa33f34f0a6136e54523d6ee
Muntakim Rahman <muntakim.rahman@gmail.com>
2024-06-09 19:35:03 -0700

    Test for Mobile and Desktop

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,7 +4,7 @@ import Muntakim_Insignia from '../../assets/images/Insignia.png'
 import Muntakim_Cursive from '../../assets/images/Muntakim_Cursive.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome, faBriefcase, faCode, faGamepad, faEnvelope, faCalendar } from '@fortawesome/free-solid-svg-icons'
-import { faGithub, faDev, faGoodreads, faLinkedin, faTwitter, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faLinkedin, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
     <div className="nav-bar">
@@ -56,22 +56,15 @@ const Sidebar = () => (
             <a target="_blank" rel="noreferrer" href="https://twitter.com/Dipto9999">
                 <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
             </a>
-            <a target="_blank" rel="noreferrer" href="mailto:dipto100@alum.ubc.ca">
-                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
-            </a>
             <a target="_blank" rel="noreferrer" href="https://www.youtube.com/channel/UCNF7p6gRuxE0dFYeDnzxoHw">
                 <FontAwesomeIcon icon={faYoutube} color="#4d4d4e" />
             </a>
-
-            {/* <a target="_blank" rel="noreferrer" href="https://www.instagram.com/dipto9999/" id="contact-start">
-                <FontAwesomeIcon icon={faInstagram} color="#4d4d4e" />
-            </a>
-            <a target="_blank" rel="noreferrer" href="https://www.goodreads.com/user/show/153601337-muntakim-rahman">
-                <FontAwesomeIcon icon={faGoodreads} color="#4d4d4e" />
+            <a target="_blank" rel="noreferrer" href="mailto:dipto100@alum.ubc.ca">
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
             </a>
             <a target="_blank" rel="noreferrer" href="https://calendly.com/muntakim-rahman">
                 <FontAwesomeIcon icon={faCalendar} color="#4d4d4e" />
-            </a> */}
+            </a>
         </li>
     </ul>
     </div>
