c4df22269436497d1f8346939673492f2bfb2e7c
Muntakim Rahman <muntakim.rahman@gmail.com>
2024-06-09 18:52:25 -0700

    Try again for Mobile

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -12,61 +12,60 @@ const Projects = () => {
             setLetterClass('text-animate-hover');
         }, 3000);
 
-        // Clear the Timeout.
         return () => clearTimeout(timeoutId);
     }, []);
 
     return (
         <>
+        <Loader type="line-scale-pulse-out"/>
         <div className='container projects-page'>
+            <span className='tags top-tags'>func main() &#123;</span>
             <div className='text-zone'>
-                <h1 class="supertitle"><AnimatedLetters
-                    letterClass={letterClass}
-                    strArray={['P', 'r', 'o', 'j', 'e', 'c', 't', ' ', 'H', 'i', 'g', 'h', 'l', 'i', 'g', 'h', 't', 's']}
-                    idx={7}
-                /></h1>
-                <p class="custom-card-text">
-                    <strong>Reflow Oven Controller <a href="https://github.com/TZlindra/ELEC291Project1Code" class="external-links">[Github Repository]</a></strong><br/>
-                    <hr class="hr-separator"/>
+                <h1 className="supertitle">
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={['P', 'r', 'o', 'j', 'e', 'c', 't', ' ', 'H', 'i', 'g', 'h', 'l', 'i', 'g', 'h', 't', 's']}
+                        idx={7}
+                    />
+                </h1>
+                <p className="custom-card-text">
+                    <strong>Reflow Oven Controller <a href="https://github.com/TZlindra/ELEC291Project1Code" className="external-links">[Github Repository]</a></strong>
+                    <hr className="hr-separator"/>
                     <iframe
                         title="Reflow Oven Controller"
                         src="https://www.youtube.com/embed/Bzm737dduOw"
-                        class="float-left"
-                        frameborder="0"
+                        className="float-left"
+                        frameBorder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowfullscreen
+                        allowFullScreen
                     ></iframe>
                     <div>
-                        An <span class="tech-highlight">8051 ASM</span> program that controls a reflow oven temperature profile for soldering <span class="tech-highlight">EFM8LB1</span> surface mount components.
-                        A <span class="tech-highlight">Tkinter</span> desktop application was developed to visualize the temperature profile and send reflow logs to a <span class="tech-highlight">Google Cloud Platform</span> server for
+                        An <span className="tech-highlight">8051 ASM</span> program that controls a reflow oven temperature profile for soldering <span className="tech-highlight">EFM8LB1</span> surface mount components.
+                        A <span className="tech-highlight">Tkinter</span> desktop application was developed to visualize the temperature profile and send reflow logs to a <span className="tech-highlight">Google Cloud Platform</span> server for
                         storage and data post-processing.
                     </div>
                 </p>
 
-                <div class="section-separator"></div>
-
-                <p class="custom-card-text">
-                    <strong>Remote Controlled, Metal Detecting Robot <a href="https://github.com/TZlindra/ELEC291Project2" class="external-links">[Github Repository]</a></strong><br/>
-                    <hr class="hr-separator"/>
+                <p className="custom-card-text">
+                    <strong>Remote Controlled, Metal Detecting Robot <a href="https://github.com/TZlindra/ELEC291Project2" className="external-links">[Github Repository]</a></strong>
+                    <hr className="hr-separator"/>
                     <iframe
                         title="Remote Controlled, Metal Detecting Robot"
                         src="https://www.youtube.com/embed/mVCBSWdCpsY"
-                        class="float-right"
-                        frameborder="0"
+                        className="float-right"
+                        frameBorder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowfullscreen
+                        allowFullScreen
                     ></iframe>
                     <div>
-                        A wirelessly controlled, battery operated robot which detects metal with principles of electromagnetic induction. The <span class="tech-highlight">C</span> firmware was implemented
-                        on an <span class="tech-highlight">STM32L0</span> microcontroller and <span class="tech-highlight">EFM8LB1</span> microcontrollers and validated with <span class="tech-highlight">matplotlib</span> data visualization.
+                        A wirelessly controlled, battery operated robot which detects metal with principles of electromagnetic induction. The <span className="tech-highlight">C</span> firmware was implemented
+                        on an <span className="tech-highlight">STM32L0</span> microcontroller and <span className="tech-highlight">EFM8LB1</span> microcontrollers and validated with <span className="tech-highlight">matplotlib</span> data visualization.
                     </div>
                 </p>
 
-                <div class="section-separator"></div>
-
-                <p class="custom-card-text contains-video">
-                    <strong>STM32 Morse Code Translator <a href="https://github.com/Dipto9999/STM32-Morse_Translator" class="external-links">[Github Repository]</a></strong><br/>
-                    <hr class="hr-separator"/>
+                <p className="custom-card-text contains-video">
+                    <strong>STM32 Morse Code Translator <a href="https://github.com/Dipto9999/STM32-Morse_Translator" className="external-links">[Github Repository]</a></strong>
+                    <hr className="hr-separator"/>
                     <video controls className="float-left">
                         <source
                             title="STM32 Morse Code Translator"
@@ -76,16 +75,14 @@ const Projects = () => {
                         <i>This video is not supported by your browser.</i>
                     </video>
                     <div>
-                        A <span class="tech-highlight">C</span> program which acquires a stream of ASCII characters from serial port via <span class="tech-highlight">UART</span>, and outputs the corresponding Morse code on
-                        an <span class="tech-highlight">STM32L4 Nucleo</span> board LED.
+                        A <span className="tech-highlight">C</span> program which acquires a stream of ASCII characters from serial port via <span className="tech-highlight">UART</span>, and outputs the corresponding Morse code on
+                        an <span className="tech-highlight">STM32L4 Nucleo</span> board LED.
                     </div>
                 </p>
 
-                <div class="section-separator"></div>
-
-                <p class="custom-card-text contains-video">
-                    <strong>Google PageRank Engine <a href="https://github.com/Dipto9999/Google_PageRank" class="external-links">[Github Repository]</a></strong><br/>
-                    <hr class="hr-separator"/>
+                <p className="custom-card-text contains-video">
+                    <strong>Google PageRank Engine <a href="https://github.com/Dipto9999/Google_PageRank" className="external-links">[Github Repository]</a></strong>
+                    <hr className="hr-separator"/>
                     <video controls className="float-right">
                         <source
                             title="Google PageRank Engine"
@@ -95,15 +92,13 @@ const Projects = () => {
                         <i>This video is not supported by your browser.</i>
                     </video>
                     <div>
-                        A simplified implementation of Google's PageRank algorithm in <span class="tech-highlight">C</span>, which ranks web pages with a <span class="tech-highlight">MATLAB</span> engine based on hyperlink structure.
+                        A simplified implementation of Google's PageRank algorithm in <span className="tech-highlight">C</span>, which ranks web pages with a <span className="tech-highlight">MATLAB</span> engine based on hyperlink structure.
                     </div>
                 </p>
 
-                <div class="section-separator"></div>
-
-                <p class="custom-card-text contains-video">
-                    <strong>Stock Portfolio App <a href="https://github.com/Dipto9999/Stock_Portfolio_App" class="external-links">[Github Repository]</a></strong><br/>
-                    <hr class="hr-separator"/>
+                <p className="custom-card-text contains-video">
+                    <strong>Stock Portfolio App <a href="https://github.com/Dipto9999/Stock_Portfolio_App" className="external-links">[Github Repository]</a></strong>
+                    <hr className="hr-separator"/>
                     <video controls className="float-left">
                         <source
                             title="Stock Portfolio App"
@@ -113,29 +108,27 @@ const Projects = () => {
                         <i>This video is not supported by your browser.</i>
                     </video>
                     <div>
-                        A <span class="tech-highlight">Tkinter</span> desktop application for monitoring and analyzing stock market trends to support investment decisions.
+                        A <span className="tech-highlight">Tkinter</span> desktop application for monitoring and analyzing stock market trends to support investment decisions.
                     </div>
                 </p>
 
-                <div class="section-separator"></div>
-
-                <div class="custom-card">
-                    <p class="custom-card-text">
-                        <strong>Data Collection App <a href="https://github.com/Dipto9999/Data_Collection_App" class="external-links">[Github Repository]</a></strong><br/>
-                        <hr class="hr-separator"/>
+                <div className="custom-card">
+                    <p className="custom-card-text">
+                        <strong>Data Collection App <a href="https://github.com/Dipto9999/Data_Collection_App" className="external-links">[Github Repository]</a></strong>
+                        <hr className="hr-separator"/>
                         <img
                             src={Data_Collection_App}
-                            class="float-right"
+                            className="float-right"
                             alt="Data Collection App"
                         />
                         <div>
-                            A <span class="tech-highlight">Django</span> web application that collects user reported COVID-19 survey data in order to make economic decisions.
+                            A <span className="tech-highlight">Django</span> web application that collects user reported COVID-19 survey data in order to make economic decisions.
                         </div>
                     </p>
                 </div>
             </div>
+            <span className='tags bottom-tags'>&#125;</span>
         </div>
-        <Loader type="line-scale-pulse-out"/>
         </>
     )
 }
