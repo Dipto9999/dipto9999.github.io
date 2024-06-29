a8f17f7fd2fc3021892f6274268ae81cc77d810a
Muntakim Rahman <muntakim.rahman@gmail.com>
2024-06-29 10:04:59 -0700

    Testing Homepage for all Environments

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -61,12 +61,14 @@ const Home = () => {
 
                 <div className="text-zone">
                     <h2 className="custom-card-text">
+                        <div className="intro-text">
+                            My name is Muntakim and I'm an Electrical Engineering undergraduate with an appetite for software development.
+                            I'm passionate about developing innovative technologies that integrate software and hardware. My experiences are in data science and embedded systems;
+                            I love working on projects that leverage the intersection of these fields! <br /><br />
+                            I've found that I thrive in environments that require first principles thinking, especially in the realm of big data. Whether working on satellite firmware or designing data-centric user interfaces,
+                            my goal is to bring robust and scalable solutions to intricate engineering challenges.
+                        </div>
                         <img src={Muntakim_Algonquin} alt="Muntakim Rahman at Algonquin Park, 2020" className="profile-img" />
-                        My name is Muntakim and I'm an Electrical Engineering undergraduate with an appetite for software development.
-                        I'm passionate about developing innovative technologies that integrate software and hardware. My experiences are in data science and embedded systems;
-                        I love working on projects that leverage the intersection of these fields! <br /><br />
-                        I've found that I thrive in environments that require first principles thinking, especially in the realm of big data. Whether working on satellite firmware or designing data-centric user interfaces,
-                        my goal is to bring robust and scalable solutions to intricate engineering challenges.
                     </h2>
                 </div>
 
