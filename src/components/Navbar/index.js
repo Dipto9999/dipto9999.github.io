5bc823ede63b510b294a5fd2c40d4bc115bd58df
Muntakim Rahman <muntakim.rahman@gmail.com>
2024-07-04 16:49:13 -0700

    Testing for Mobile with Padding

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -29,34 +29,37 @@ const Navbar = () => (
             </nav>
             <ul className="social-links">
                 <li>
-                    <a target="_blank" rel="noreferrer" href="https://github.com/Dipto9999" className="social-icon">
+                    <a target="_blank" rel="noreferrer" href="https://github.com/Dipto9999" className="social-icons">
                         <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
                     </a>
                 </li>
                 <li>
-                    <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/muntakim-rahman/" className="social-icon">
+                    <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/muntakim-rahman/" className="social-icons">
                         <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
                     </a>
                 </li>
                 <li>
-                    <a target="_blank" rel="noreferrer" href="https://twitter.com/Dipto9999" className="social-icon">
+                    <a target="_blank" rel="noreferrer" href="https://twitter.com/Dipto9999" className="social-icons">
                         <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
                     </a>
                 </li>
                 <li>
-                    <a target="_blank" rel="noreferrer" href="https://www.youtube.com/channel/UCNF7p6gRuxE0dFYeDnzxoHw" className="social-icon">
+                    <a target="_blank" rel="noreferrer" href="https://www.youtube.com/channel/UCNF7p6gRuxE0dFYeDnzxoHw" className="social-icons">
                         <FontAwesomeIcon icon={faYoutube} color="#4d4d4e" />
                     </a>
                 </li>
                 <li>
-                    <a target="_blank" rel="noreferrer" href="mailto:dipto100@alum.ubc.ca" className="social-icon">
+                    <a target="_blank" rel="noreferrer" href="mailto:dipto100@alum.ubc.ca" className="social-icons">
                         <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
                     </a>
                 </li>
                 <li>
-                    <a target="_blank" rel="noreferrer" href="https://calendly.com/muntakim-rahman" className="social-icon">
+                    <a target="_blank" rel="noreferrer" href="https://calendly.com/muntakim-rahman" className="social-icons">
                         <FontAwesomeIcon icon={faCalendar} color="#4d4d4e" />
                     </a>
+                </li>
+                <li>
+                    <span id="icon-padding"><br/></span>
                 </li>
             </ul>
         </div>
