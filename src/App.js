982f16c926177b73125be8a962c917de0b56ab82
Muntakim Rahman <muntakim.rahman@gmail.com>
2024-07-13 14:55:21 -0700

    Testing Out Footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
+import Footer from './components/Footer';
 import Home from './components/Home';
 import Experiences from './components/Experiences';
 import Projects from './components/Projects';
@@ -15,6 +16,7 @@ function App() {
           <Route path="projects" element={<Projects />} />
         </Route>
       </Routes>
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import './index.scss';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Muntakim Rahman : Software Engineer | &copy; {new Date().getFullYear()}</p>
+        </footer>
+    )
+}
+
+export default Footer;
